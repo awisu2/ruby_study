5983d8a26ca9f1495b0f730c18ac58035196a795
awisu2 <awisu2@gmail.com>
2019-05-07 13:16:18 +0900

    シンボルのサンプルを追加

--- index.rb
+++ index.rb
@@ -1,3 +1,4 @@
+# stdout ==========
 # 標準出力は3種類
 ## p: 引数を展開して出力、文字列化 returnあり
 ## puts: 引数を展開して出力、改行あり、returnなし
@@ -28,8 +29,7 @@ end
 stdout(Sample1.new('hello world 1'))
 stdout(Sample1.new('hello world 2'))
 
-# args
-
+# args ==========
 # 大文字から始まる値はconst
 CONST1 = 'const'
 Const2 = 'const'
@@ -38,6 +38,13 @@ def args
   $global = 'global set in def'
   p '- const1', $CONST1
   p '- const2', $Const2
+
+  # :をつけるとシンボル変数
+  # 文字列に対応した固定アドレスがglobalに登録されておりそれが使い回される(object_idが同一)
+  # (通常の文字列は宣言ごとに変化、というかstringのインスタンスが生成される)
+  # 変更不可、どこで参照しても同一のものと判定される
+  # ハッシュのキーで利用する場合:を省略可能
+  simbol = :simbol
 end
 $global = 'global'
 local = 'local'
@@ -48,7 +55,7 @@ p '- global', $global
 puts '<<< args <<<<<'
 print "\n" # 調整用の改行出力
 
-# nums
+# nums ==========
 def nums
   puts '>>> nums >>>>>'
   x = 1
@@ -62,7 +69,7 @@ def nums
 end
 nums
 
-# nums
+# arr ==========
 def arr
   puts '>>> arr >>>>>'
 
@@ -78,7 +85,7 @@ def arr
 end
 arr
 
-# def(関数)
+# def(関数) ==========
 def def_sample
   puts '>>> def >>>>>'
   # 引数がない場合は[呼び出す場合も]()を記述しなくて良い
@@ -107,7 +114,7 @@ def def_sample
 end
 def_sample
 
-# class
+# class ==========
 class Sample2
   # コンストラクタ
   def initialize(word)
@@ -140,4 +147,4 @@ def class_sample
 end
 class_sample
 
-# % 記法
+# % 記法 ==========
